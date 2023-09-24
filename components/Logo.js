b01d18d42c8d44af67a19df086fdb8eabacc4f0a
Mille Brekke Amundsen <deimile7@gmail.com>
2023-09-24 05:57:51 +0200

    Update Logo.js

--- components/Logo.js
+++ components/Logo.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
     return (
         <div className='logo w-16 h-16'>
-            <Image src='/image/icon.png' alt='logo' width={1000} height={1000}/>
+            <Link href='/'><Image src='/image/icon.png' alt='logo' width={1000} height={1000}/></Link>
         </div>
     )
 }
