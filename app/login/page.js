630fdf0ee3b065ef2f99b3fec21c01829abfffea
Mille Brekke Amundsen <deimile7@gmail.com>
2023-09-24 06:35:46 +0200

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -44,7 +44,7 @@ export default function LoginPage() {
                         />
                     </div>
                     <div className="mb-4">
-                        <button type="submit" className="w-full bg-teal-500 text-white font-mono py-2 px-6 rounded hover:bg-teal-800 duration-500 rounded-md">Login</button>
+                        <button type="submit" className="w-full bg-teal-500 text-white font-mono py-2 px-6 rounded hover:bg-teal-800 duration-500">Login</button>
                     </div>
                 </form>
             </div>
